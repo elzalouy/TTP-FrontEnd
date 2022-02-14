fb2b086898d78bbc37518c1bc03fa469ba695b34
Moezzain <moezzain94@gmail.com>
2022-02-14 21:35:28 +0300

    theme configuration done v2

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,15 +1,15 @@
 import { combineReducers } from "redux";
 import storage from "redux-persist/lib/storage";
-import { USER_LOGOUT } from "./auth/types";
+// import { USER_LOGOUT } from "./auth/types";
 
 export const appReducer = combineReducers({
 
 });
 
 export const rootReducer = (state : any , action : any) => {
-  if (action.type === USER_LOGOUT) {
+  // if (action.type === USER_LOGOUT) {
       storage.removeItem('persist:root');
       return appReducer(undefined, action);
-  }
+  // }
 };
 
