f7c93cf846fc7cd32b126fde1bc1c062c07aae7b
= <almutawasaud1@hotmail.com>
2022-02-19 15:54:16 +0300

    Category-Page, Category-Popup and PM-Popup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import Login from "./pages/AuthPage/login";
 import RestPassword from "./pages/AuthPage/rest";
 import Forget from "./pages/AuthPage/forget";
 import Projects from "./pages/Projects/projects";
-import departments from "./pages/Departments/departments";
+import Departments from "./pages/Departments/departments";
+import Category from "./pages/Category/Category";
+import ProjectManagers from "./pages/Project managers/ProjectManagers";
 
 type Props = {};
 
@@ -18,7 +20,9 @@ const App: React.FC<Props> = () => {
           <Route path="/ForgetPassword" component={Forget} />
           <Route path="/RestPassword" component={RestPassword} />
           <Route path="/Projects" component={Projects} />
-          <Route path="/Departments" component={departments} />
+          <Route path="/Departments" component={Departments} />
+          <Route path="/Categories" component={Category} />
+          <Route path="/ProjectManagers" component={ProjectManagers} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Category/CreateCategoryPopUp.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./Category.css";
+import "./../Project managers/Pmcard.css";
+
+const CreateCategoryPopUp = (props) => {
+  const [mainCategory, setMainCategory] = useState("");
+  const [subCategory, setSubCategory] = useState("");
+  const [subCategories, setsubCategories] = useState([]);
+  const onMainChange = (e) => {
+    setMainCategory(...mainCategory, e.target.value);
+  };
+  const onSubChange = (e) => {
+    setSubCategory(e.target.value);
+  };
+  const addSubCategory = (e) => {
+    if (subCategories.length === 0) setsubCategories([subCategory]);
+    else {
+      setsubCategories([...subCategories, subCategory]);
+    }
+    setSubCategory("");
+  };
+
+  const removeSubCategory = (subCategory) => {
+    setsubCategories(
+      subCategories.filter((element) => element !== subCategory)
+    );
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="Pm-popup">
+      <div className="popup-header">
+        <h4>Add new category</h4>
+        <span className="close-icon" onClick={props.handleClose}>
+          x
+        </span>
+      </div>
+      <form className="form-inputs">
+        <label className="label">Main category</label>
+        <div className="f-inputs" style={{ display: "flex" }}>
+          <input
+            className="input-auth"
+            type="text"
+            name="mainCategory"
+            placeholder="Category name"
+            value={mainCategory}
+            onChange={onMainChange}
+          />
+        </div>
+        <label className="label">Sub-Category</label>
+
+        <div className="f-inputs" style={{ display: "flex" }}>
+          <input
+            className="input-auth"
+            type="text"
+            name="subCategory"
+            value={subCategory}
+            onChange={onSubChange}
+            placeholder="Sub category"
+          />
+          <div className="add-subcategory" onClick={addSubCategory}>
+            Add
+          </div>
+        </div>
+        <div className="subcategories">
+          {subCategories.map((subcategory) => (
+            <div
+              className="subcategory"
+              key={subcategory}
+              onClick={() => {
+                removeSubCategory(subcategory);
+              }}
+            >
+              {subcategory}
+            </div>
+          ))}
+        </div>
+        <div className="popup-btn">
+          <button className="btn-cancel" onClick={props.handleClose}>
+            Cancel
+          </button>
+          <button className="btn-auth" onClick={handleSubmit}>
+            Done
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCategoryPopUp;
