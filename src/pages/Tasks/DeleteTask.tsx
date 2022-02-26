7d29a22bc1129eb4fed72fea107d6bac4d12ffa1
Mahadi Hasan <86624268+mhasancy@users.noreply.github.com>
2022-02-26 15:42:09 +0600

    delete task separated

--- src/pages/Tasks/DeleteTask.tsx
+++ src/pages/Tasks/DeleteTask.tsx
@@ -1,6 +1,7 @@
 import { Box, IconButton } from "@mui/material";
 import React, { useState } from "react";
 import IMAGES from "../../assets/img";
+import PopUp from "../../coreUI/usable-component/popUp";
 import "./DeleteTask.css";
 
 type Props = {};
@@ -19,6 +20,26 @@ const DeleteTask: React.FC<Props> = () => {
           <img src={IMAGES.deleteicon} alt="sortout" />
         </IconButton>
       </Box>
+
+      <PopUp show={Show} widthSize="23vw">
+        <p className="delete-title">
+          Are you sure you want to delete this task?
+        </p>
+        <hr className="hr-dt" />
+        <div className="controllers-dt">
+          <button
+            className="cancelBtn-dt"
+            onClick={() => {
+              setShow("none");
+            }}
+          >
+            Cancel
+          </button>
+          <button className="redBtn" onClick={() => {}}>
+            Delete
+          </button>
+        </div>
+      </PopUp>
     </Box>
   );
 };
