e3d13bcec26d9e76f8cec45d238f4a3db00608fe
Mahadi Hasan <86624268+mhasancy@users.noreply.github.com>
2022-02-26 15:35:02 +0600

    clients page separated

--- src/pages/Clients/CreateNewClient.tsx
+++ src/pages/Clients/CreateNewClient.tsx
@@ -1,7 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React, { useState } from "react";
 import IMAGES from "../../assets/img";
-import PopUp from "../../coreUI/usable-component/popUp";
 import "./CreateNewClient.css";
 
 type Props = {};
@@ -21,50 +20,6 @@ const CreateNewClient: React.FC<Props> = () => {
         <img src={IMAGES.plus} alt="add" />
         <Typography>Create new client</Typography>
       </Box>
-
-      <PopUp show={Show} widthSize="30vw">
-        <Box sx={{ paddingLeft: "15px" }}>
-          <Box>
-            <img
-              className="closeIcon"
-              width="9"
-              height="9"
-              src={IMAGES.closeicon}
-              alt="closeIcon"
-              onClick={() => {
-                setShow("none");
-              }}
-            />
-          </Box>
-
-          <Typography className="new-client-title">Add new client</Typography>
-          <Box style={{ marginBottom: "20px", marginTop: "20px" }}>
-            <img src={IMAGES.imgupload} alt="" />
-          </Box>
-          <label className="label-client">Client Name</label>
-          <input
-            className="input-client"
-            type="text"
-            placeholder="Ex: Ahmad Ali"
-          />
-
-          <br />
-
-          <Box className="controllers">
-            <button
-              className="cancelBtn"
-              onClick={() => {
-                setShow("none");
-              }}
-            >
-              Cancel
-            </button>
-            <button className="blackBtn" onClick={() => {}}>
-              Done
-            </button>
-          </Box>
-        </Box>
-      </PopUp>
     </>
   );
 };
