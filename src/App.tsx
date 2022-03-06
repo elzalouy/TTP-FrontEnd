0672b2335904b268d9eb0318abe555890e99810e
mohamednageh7 <mohamednageh7@yahoo.com>
2022-03-06 21:23:42 +0300

    push to heroku

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App: React.FC<Props> = () => {
             path="/projectManagers"
             component={ProjectManagers}
           />
-        </Switch>
+        </Switch>  
       </BrowserRouter>
     </div>
   );
