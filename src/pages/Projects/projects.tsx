06835297787f7b7ec30dc5c3b26f0ed1ea6266c2
saudalmutawa <almutawasaud1@hotmail.com>
2022-03-17 10:10:27 +0300

    css files added - add props

--- src/pages/Projects/projects.tsx
+++ src/pages/Projects/projects.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import CreateNewProject from "./createNewProject";
-=======
+// <<<<<<< HEAD
+// import CreateNewProject from "./createNewProject";
+// =======
 import CreateNewProject from "./newProject";
->>>>>>> origin/development
+
 import IMAGES from "../../assets/img/index";
 import "../Departments/departments.css";
 import SearchBar from "../Category/SearchBar";
@@ -20,91 +20,89 @@ const Projects: React.FC = () => {
   const inProgressProjects = useAppSelector(selectInprogressProjects);
   const doneProjects = useAppSelector(selectDoneProjects);
   return (
-<<<<<<< HEAD
-   <Box sx={{ width: "100%" }}>
-=======
->>>>>>> origin/development
-    <div className="departments-page">
-      <h2 className="departments-title">Projects</h2>
-      <div
-        className="department-tools"
-        style={{
-          marginTop: "2%",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <div className="filter-icon">
-          <img src={IMAGES.filtericon} alt="FILTER" />
-        </div>
-        <div>
-          <label>Sort by:</label>
-          <select className="select-filter" name="color">
-            <option value="Due Datee">Due Date</option>
-            <option value="In progress">In progress</option>
-            <option value="To do">To do</option>
-          </select>
-        </div>
-        <div>
-          <label style={{ padding: 0 }}>Project manager:</label>
-          <select className="select-filter" style={{}} name="color">
-            <option value="Nawaf m">Nawaf M</option>
-            <option value="option 2">option 2</option>
-            <option value="option 3">option 3</option>
-          </select>
-        </div>
-        <div>
-          <label style={{ padding: 0 }}>Project team:</label>
-          <select className="select-filter" name="color">
-            <option value="Developers team">Developers team</option>
-            <option value="option 2">option 2</option>
-            <option value="option 3">option 3</option>
-          </select>
-        </div>
-        <div>
-          <select className="select-filter" name="color">
-            <option value="">Client name</option>
-            <option value="option 2">option 2</option>
-            <option value="option 3">option 3</option>
-          </select>
-        </div>
-        <div>
-          <select className="select-filter" name="color">
-            <option value="">Status</option>
-            <option value="option 2">option 2</option>
-            <option value="option 3">option 3</option>
-          </select>
-        </div>
-        <div>
-          <SearchBar />
-        </div>
-      </div>
-      <Box sx={{ mt: 2, display: "flex", flexDirection: "column" }}>
-        <CreateNewProject />
-        <ProjectCard status={"In progress"} Projects={inProgressProjects} />
-        <ProjectCard status={"Done"} Projects={doneProjects} />
-        <Box
-          sx={{
+
+    <Box sx={{ width: "100%" }}>
+
+      <div className="departments-page">
+        <h2 className="departments-title">Projects</h2>
+        <div
+          className="department-tools"
+          style={{
+            marginTop: "2%",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            borderRadius: "12px",
-            backgroundColor: "#F1F1F4",
-            p: 1,
-            cursor: "pointer",
-            font: "normal normal 600 16px/30px Cairo",
-            color: "#909090",
+            justifyContent: "space-between",
           }}
         >
-          <RotateRightIcon></RotateRightIcon> Loading More
+          <div className="filter-icon">
+            <img src={IMAGES.filtericon} alt="FILTER" />
+          </div>
+          <div>
+            <label>Sort by:</label>
+            <select className="select-filter" name="color">
+              <option value="Due Datee">Due Date</option>
+              <option value="In progress">In progress</option>
+              <option value="To do">To do</option>
+            </select>
+          </div>
+          <div>
+            <label style={{ padding: 0 }}>Project manager:</label>
+            <select className="select-filter" style={{}} name="color">
+              <option value="Nawaf m">Nawaf M</option>
+              <option value="option 2">option 2</option>
+              <option value="option 3">option 3</option>
+            </select>
+          </div>
+          <div>
+            <label style={{ padding: 0 }}>Project team:</label>
+            <select className="select-filter" name="color">
+              <option value="Developers team">Developers team</option>
+              <option value="option 2">option 2</option>
+              <option value="option 3">option 3</option>
+            </select>
+          </div>
+          <div>
+            <select className="select-filter" name="color">
+              <option value="">Client name</option>
+              <option value="option 2">option 2</option>
+              <option value="option 3">option 3</option>
+            </select>
+          </div>
+          <div>
+            <select className="select-filter" name="color">
+              <option value="">Status</option>
+              <option value="option 2">option 2</option>
+              <option value="option 3">option 3</option>
+            </select>
+          </div>
+          <div>
+            <SearchBar />
+          </div>
+        </div>
+        <Box sx={{ mt: 2, display: "flex", flexDirection: "column" }}>
+          <CreateNewProject />
+          <ProjectCard status={"In progress"} Projects={inProgressProjects} />
+          <ProjectCard status={"Done"} Projects={doneProjects} />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              borderRadius: "12px",
+              backgroundColor: "#F1F1F4",
+              p: 1,
+              cursor: "pointer",
+              font: "normal normal 600 16px/30px Cairo",
+              color: "#909090",
+            }}
+          >
+            <RotateRightIcon></RotateRightIcon> Loading More
+          </Box>
         </Box>
-      </Box>
-    </div>
-<<<<<<< HEAD
-   </Box>
-=======
->>>>>>> origin/development
+      </div>
+
+    </Box>
+
   );
 };
 
