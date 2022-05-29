5d9d386207aa6eb87a96a47af7bd310e083b147a
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-05-29 16:10:34 +0200

    fix

--- src/config/socket/actions.ts
+++ src/config/socket/actions.ts
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
 import apiUrl from "../../services/api.json";
-export let socket = io(apiUrl.SOCKET_BASE_URL, { withCredentials: true });
+export let socket = io(apiUrl.SOCKET_BASE_URL);
