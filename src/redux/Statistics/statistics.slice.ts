7d8976f9e334881fcf8561d2238499f82484609e
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-07-24 15:22:33 +0200

    Fix overview issues

--- src/redux/Statistics/statistics.slice.ts
+++ src/redux/Statistics/statistics.slice.ts
@@ -17,12 +17,12 @@ const StatisticsSlice: Slice<StatisticsInterface> = createSlice({
       state = StatisticsState,
       action: PayloadAction<any>
     ) => {
-      // get projects and tasks and the user
+      // get projects, tasks and the user
       let projects: Project[] = action.payload.projects;
       let tasks: Task[] = action.payload.tasks;
       let user: User = action.payload.user;
 
-      //check the user
+      // check the user
       if (user && user?.role?.length > 0) {
         if (projects && projects.length > 0) {
           state.OM.projectsCloseToDeadlines = projects.filter(
@@ -31,6 +31,7 @@ const StatisticsSlice: Slice<StatisticsInterface> = createSlice({
               isCloseToDeadline(item.projectDeadline, item.startDate, 35)
           );
         }
+
         if (tasks && tasks.length > 0) {
           let inprogress = tasks.filter((item) => item.status === "inProgress");
           let taskBoard = tasks.filter((item) => item.status === "Tasks Board");
@@ -55,40 +56,46 @@ const StatisticsSlice: Slice<StatisticsInterface> = createSlice({
       state = StatisticsState,
       action: PayloadAction<any>
     ) => {
+      // get projects, tasks, and users
       let projects: Project[] = action.payload.projects;
       let tasks: Task[] = action.payload.tasks;
       let user: User = action.payload.user;
-      let userProjects =
-        projects &&
-        projects?.filter((item) => item.projectManager?._id === user._id);
-      state.PM.projects = userProjects;
-      if (tasks && tasks.length > 0) {
-        let ids = userProjects.flatMap((item: Project) => item._id);
-        let review = tasks.filter(
-          (item) => ids.includes(item.projectId) && item.deadline === "Review"
-        );
-        let shared = tasks.filter(
-          (item) => ids.includes(item.projectId) && item.deadline === "Shared"
-        );
-        let inprogress = tasks.filter(
-          (item) => ids.includes(item.projectId) && item.status === "inProgress"
-        );
-        state.PM.shared = shared;
-        state.PM.reviewLength = review.length;
-        state.PM.sharedLength = shared.length;
-        state.PM.inProgress = setTasksToArrays(inprogress);
-        state.PM.review = setTasksToArrays(review);
-        state.PM.tasksCloseToDeadline = tasks.filter(
-          (item) =>
-            ids.includes(item.projectId) &&
-            item.deadline &&
-            isCloseToDeadline(item.deadline, item.start, 25)
-        );
-        state.PM.projectsCloseToDeadlines = userProjects?.filter(
-          (item) =>
-            item?.projectDeadline &&
-            isCloseToDeadline(item.projectDeadline, item.startDate, 35)
-        );
+
+      if (user && user?.role?.length > 0) {
+        let userProjects =
+          projects &&
+          projects?.filter((item) => item.projectManager?._id === user._id);
+        state.PM.projects = userProjects;
+
+        if (tasks && tasks.length > 0) {
+          let ids = userProjects.flatMap((item: Project) => item._id);
+          let review = tasks.filter(
+            (item) => ids.includes(item.projectId) && item.status === "Review"
+          );
+          let shared = tasks.filter(
+            (item) => ids.includes(item.projectId) && item.status === "Shared"
+          );
+          let inprogress = tasks.filter(
+            (item) =>
+              ids.includes(item.projectId) && item.status === "inProgress"
+          );
+          state.PM.shared = shared;
+          state.PM.reviewLength = review.length;
+          state.PM.sharedLength = shared.length;
+          state.PM.inProgress = setTasksToArrays(inprogress);
+          state.PM.review = setTasksToArrays(review);
+          state.PM.tasksCloseToDeadline = tasks.filter(
+            (item) =>
+              ids.includes(item.projectId) &&
+              item.deadline &&
+              isCloseToDeadline(item.deadline, item.start, 25)
+          );
+          state.PM.projectsCloseToDeadlines = userProjects?.filter(
+            (item) =>
+              item?.projectDeadline &&
+              isCloseToDeadline(item.projectDeadline, item.startDate, 35)
+          );
+        }
       }
       state.loading = false;
     },
