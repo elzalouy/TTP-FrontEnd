8ea2fd3f266d2742323cd26ca47648b645754a75
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-08-24 20:34:51 +0200

    Last update / Clients, Projects Filter + solving Edit Task

--- src/views/Clients/Read/ClientCard.tsx
+++ src/views/Clients/Read/ClientCard.tsx
@@ -2,9 +2,9 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { Client as IClient } from "./Clients";
-import TaskCounterContainer from "../Read/ClientCard/Containers/TaskCounterContainer";
-import ProjectCounterContainer from "../Read/ClientCard/Containers/ProjectCounterContainer";
-import Header from "./ClientCard/Header";
+import TaskCounterContainer from "../Read/Card/Containers/TaskCounterContainer";
+import ProjectCounterContainer from "../Read/Card/Containers/ProjectCounterContainer";
+import Header from "./Card/Header";
 
 interface IProps {
   client: IClient;
@@ -13,7 +13,6 @@ interface IProps {
 const ClientCard: React.FC<IProps> = ({ client }) => {
   const { _id, doneProject, inProgressProject, image } = client;
   const [preview, setPreview] = useState("");
-
 
   useEffect(() => {
     if (image?.size && image?.name) {
@@ -31,10 +30,14 @@ const ClientCard: React.FC<IProps> = ({ client }) => {
         data-test-id="client-card"
       >
         <Header client={client} preview={preview} />
-        <ProjectCounterContainer done={doneProject} client={client} inProgressProject={inProgressProject} />
+        <ProjectCounterContainer
+          done={doneProject}
+          client={client}
+          inProgressProject={inProgressProject}
+        />
         <TaskCounterContainer id={_id} />
       </Grid>
-    </Box >
+    </Box>
   );
 };
 export default ClientCard;
