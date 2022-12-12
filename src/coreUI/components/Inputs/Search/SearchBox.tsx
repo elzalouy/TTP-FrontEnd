d58d729f6017f9d6bbc075ead5e40f84b732cee8
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-12-12 16:10:49 +0200

    fix Clients select

--- src/coreUI/components/Inputs/Search/SearchBox.tsx
+++ src/coreUI/components/Inputs/Search/SearchBox.tsx
@@ -5,7 +5,11 @@ import "./Search.css";
 
 const Search: React.FC<ISearch> = ({ onChange, placeholder, value, size }) => {
   return (
-    <div className={"core-ui-search-wrapper"+ " " + "core-ui-search-wrapper-"+size}>
+    <div
+      className={
+        "core-ui-search-wrapper" + " " + "core-ui-search-wrapper-" + size
+      }
+    >
       <img src={IMAGES.search} className="core-ui-search-icon" />
       <input
         className={"core-ui-search" + " " + size}
